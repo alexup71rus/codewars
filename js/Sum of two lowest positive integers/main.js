b254393fe5a6202510685bceb05ba6e76f195cb9
alexup71rus <42513019+alexup71rus@users.noreply.github.com>
2019-08-10 00:24:49 +0300

    update

--- js/Sum of two lowest positive integers/main.js
+++ js/Sum of two lowest positive integers/main.js
@@ -1,14 +1,4 @@
 function sumTwoSmallestNumbers(numbers) {
-  const smallestNumber = Math.min.apply(Math, numbers);
-  let twoSmallestNumber = Math.max.apply(Math, numbers);
-  numbers.forEach((el) => {
-    twoSmallestNumber = el && (smallestNumber < el && twoSmallestNumber > el)
-      ? el
-      : twoSmallestNumber;
-  });
-  return twoSmallestNumber !== -Infinity
-    ? smallestNumber + twoSmallestNumber
-    : smallestNumber !== -Infinity
-      ? smallestNumber
-      : 0;
+  numbers = numbers.sort(function (a, b) { return a - b; });
+  return numbers[0] + numbers[1];
 }
